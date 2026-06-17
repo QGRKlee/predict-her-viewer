d6cf29a50eb9096df62a582c2f7bbe4aa03761e0
Klee Irwin <Klee@omninutra.com>
2026-06-16 15:09:32 -1000

    Refresh viewer without startup lunar visuals

---
+++ app/assets/index-Bgl1Bnft.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-y6764_dH.js","assets/index-e9z-167c.js","assets/astro-CKYIcK6G.js","assets/three-Bv1OsDGp.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./astro-CKYIcK6G.js";import{r as o}from"./index-e9z-167c.js";import"./three-Bv1OsDGp.js";const s=o("Browser",{web:()=>e(()=>import("./web-y6764_dH.js"),__vite__mapDeps([0,1,2,3])).then(r=>new r.BrowserWeb)});export{s as Browser};
