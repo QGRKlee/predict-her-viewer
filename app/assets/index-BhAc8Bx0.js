2c7c3bd63e7d9164bee3a37fdabd3c7ac209c6f9
Klee Irwin <Klee@omninutra.com>
2026-06-16 14:26:33 -1000

    Refresh viewer from latest app build

---
+++ app/assets/index-BhAc8Bx0.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DQSOmkJA.js","assets/index-CpPNaHe5.js","assets/astro-CKYIcK6G.js","assets/three-Bv1OsDGp.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./astro-CKYIcK6G.js";import{r as o}from"./index-CpPNaHe5.js";import"./three-Bv1OsDGp.js";const s=o("Browser",{web:()=>e(()=>import("./web-DQSOmkJA.js"),__vite__mapDeps([0,1,2,3])).then(r=>new r.BrowserWeb)});export{s as Browser};
